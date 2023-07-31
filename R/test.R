8f4e6df35a6605da1d92f01751c792e97653faa2
U_M191W <glebivanchenko@bayestech.ru>
2023-07-31 08:54:14 +0300

    Start

--- R/test.R
+++ R/test.R
@@ -1,6 +1,6 @@
 source("./BondsFundamentalsAPI.R")
 
-api_token <- "demo"
+api_token <- "64bce94f81ae02.82666688"
 isin <- "DE000CB83CF0"
 
 result <- get_bonds_fundamentals_data(api_token = api_token, isin = isin)
