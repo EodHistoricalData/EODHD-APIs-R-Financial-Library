88e72348f786e1bdedb1458dd88a61372530c041
Nawado <44095746+Nawado@users.noreply.github.com>
2023-08-22 12:23:24 +0300

    Update BaseAPI.R

--- R/BaseAPI.R
+++ R/BaseAPI.R
@@ -2,7 +2,7 @@ library(httr)
 library(jsonlite)
 
 rest_get_method <- function(api_key, endpoint = "", uri = "", querystring = list()) {
-  api_url <- "https://eodhistoricaldata.com/api"
+  api_url <- "https://eodhd.com/api"
   
     url = paste0(api_url, "/", endpoint, "/", uri)
     querystring$fmt = "json"
